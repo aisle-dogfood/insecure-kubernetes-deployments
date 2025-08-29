b1bc9fd1ac7d03ccddcaab8f582f472e354da4f4
weareaisle-demo-app <220225522+weareaisle-demo-app@users.noreply.github.com>
2025-08-29 09:07:29 +0000

    Apply fix for issue: Subprocess Shell Injection in app.py at line 83

--- insecure-app/app.py
+++ insecure-app/app.py
@@ -80,7 +80,7 @@ def result():
     # 2 - Command Injection
     if 'command' in request.form:
         cmd = request.form['command']
-        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         if process.returncode == 0:
             output = stdout.decode('utf-8')
